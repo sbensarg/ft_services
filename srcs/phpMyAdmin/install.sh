f0ba0cfe07d19f565fac65937f6b9cba134afebc
Sara Bensarghin <sbensarg@e1r9p2.1337.ma>
2021-04-15 16:36:45 +0000

    phpmyadmin

--- srcs/phpMyAdmin/install.sh
+++ srcs/phpMyAdmin/install.sh
@@ -6,31 +6,21 @@
 #    By: sbensarg <sbensarg@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/13 16:21:57 by sbensarg          #+#    #+#              #
-#    Updated: 2021/04/15 16:04:20 by sbensarg         ###   ########.fr        #
+#    Updated: 2021/04/15 16:05:45 by sbensarg         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-apk update 
-
 #install the Nginx web server
+apk update && apk upgrade 
 apk add nginx
 apk add openrc
 openrc default
-#create the user named u1 and Ngnix directory
-#adduser -g 'Nginx www user' -h /home/mynginx u1 -D
-# Nginx configuration
-# create a virtual hosts configuration file of Nginx server
-mkdir /home/mynginx
+apk update && apk upgrade
+apk add unit unit-php7 
+apk add php7 php7-fpm php7-mysqli php7-opcache php7-gd php7-mysqli php7-zlib php7-curl php7-phar php7-json php7-mbstring
+rc-update add php-fpm7 default
+rc-update add nginx default
 mv default.conf /etc/nginx/conf.d
-mv index.html /home/mynginx/
-# cd /etc/ngnix/conf.d
-# vim default.conf
-# cd /home/mynginx
-# vim index.html
-#chown -R u1:u1 home
-# rc-service nginx restart
-
-
-# mv www.mytest.com.conf /etc/nginx/conf.d/
+mv index.php /var/www/localhost/htdocs
